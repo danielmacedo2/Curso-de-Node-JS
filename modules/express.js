348b40f7d3419a0d07d24ec0aa49b49df735f344
Daniel Macedo <danielmsv2011@hotmail.com>
2022-04-10 19:21:46 -0300

    remove and update adicionado

--- modules/express.js
+++ modules/express.js
@@ -6,35 +6,60 @@ const app = express();
 
 app.use(express.json()); // vai sinalizar que vamos utilizar Json nas nossas requiçõe
 
+app.get("/users", async (req, res) => {
+  // Get para pegar uma lista de usúarios
+  try {
+    const users = await UserModel.find({});
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1>Hello World! </h1>");
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
-app.get("/users", (req, res) => {
-  // Get para pegar uma lista de usúarios
-  const users = [
-    {
-      name: "Joana Doe",
-      email: "joanadoe@gmail.com",
-    },
-    {
-      name: "Daniel Macedo",
-      email: "danielms@gmail.com",
-    },
-  ];
+app.get('/users/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
 
-  res.status(200).json(users);
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
 
-    res.status(500).json(user)
+    res.status(500).json(user);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
